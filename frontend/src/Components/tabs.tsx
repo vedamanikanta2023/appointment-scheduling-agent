1fac6ef433901dcf7da4a08c46eca7ed32e910f8
vedamanikanta <manikanta.vedam@gmail.com>
2026-06-01 17:41:56 +0530

    code formatted

--- frontend/src/Components/tabs.tsx
+++ frontend/src/Components/tabs.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-const Tab1 = ()=><div>Tab 1 conteent successful</div>
+const Tab1 = () => <div>Tab 1 conteent successful</div>;
 
-const Tab2 = ()=>{
-    throw new Error('Intensionally falling in error')
-}
+const Tab2 = () => {
+  throw new Error("Intensionally falling in error");
+};
 
 export const Tabs = () => {
   const [activeTab, setActiveTab] = useState(0);
@@ -13,13 +13,21 @@ export const Tabs = () => {
       <div>Tabs</div>
       <div>
         <div>
-          <button onClick={()=>setActiveTab(0)}>Tab 1</button>
+          <button onClick={() => setActiveTab(0)}>Tab 1</button>
         </div>
         <div>
-          <button onClick={()=>setActiveTab(1)}>Tab 2</button>
+          <button onClick={() => setActiveTab(1)}>Tab 2</button>
         </div>
       </div>
-      {!activeTab ? <div><Tab1 /></div> : <div>Tab2 Content <Tab2 /></div>}
+      {!activeTab ? (
+        <div>
+          <Tab1 />
+        </div>
+      ) : (
+        <div>
+          Tab2 Content <Tab2 />
+        </div>
+      )}
     </div>
   );
 };
