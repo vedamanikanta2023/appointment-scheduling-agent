e9c3ef67f223c59da854d9ab7ef531b77296e48e
vedamanikanta <manikanta.vedam@gmail.com>
2026-06-25 13:14:39 +0530

    hii

--- frontend/src/Components/tabs.tsx
+++ frontend/src/Components/tabs.tsx
@@ -1,6 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const Tab1 = () => <div>Tab 1 conteent successful</div>;
+const Tab1 = () => {
+  useEffect(() => {
+    const handleResize = throttle(() => {
+      console.log("throttling");
+      console.log(window.innerWidth);
+    }, 500);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return <div>Tab 1 content successful</div>;
+};
 
 const Tab2 = () => {
   throw new Error("Intensionally falling in error");
@@ -31,3 +46,29 @@ export const Tabs = () => {
     </div>
   );
 };
+
+function throttle(fn: Function, delay = 300) {
+  let last = 0;
+  let throttleTimeout: any;
+
+  return function (...args: any[]) {
+    const now = Date.now();
+
+    if (now - last < delay) {
+      clearTimeout(throttleTimeout);
+
+      throttleTimeout = setTimeout(
+        () => {
+          last = Date.now();
+          fn(...args);
+        },
+        delay - (now - last),
+      );
+
+      return;
+    }
+
+    last = now;
+    fn(...args);
+  };
+}
