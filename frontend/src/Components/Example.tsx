e3355ade206952a111edf1adb05e965dba995664
vedamanikanta <manikanta.vedam@gmail.com>
2026-07-09 23:10:59 +0530

    checking child updating parent component

--- frontend/src/Components/Example.tsx
+++ frontend/src/Components/Example.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useLayoutEffect, useRef,useState } from "react";
 
 export function Example() {
   const [inputs, setInputs] = useState([]);
@@ -12,6 +12,7 @@ export function Example() {
   
   return (
     <div>
+      <Parent />
       <button
         onClick={() => {
           setInputs((p) => [...p, String(Math.random() * 10)]);
@@ -31,4 +32,32 @@ export function Example() {
       })}
     </div>
   );
+}
+
+function Parent() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return<>
+  <Reffing/>
+  <p>{count}</p>
+  <Child onClick={increment} />
+  </> ;
+}
+
+function Child({ onClick }:{onClick:React.DOMAttributes<HTMLButtonElement>}) {
+  return <button onClick={onClick}>Increment 99999</button>;
+}
+
+function Reffing() {
+  const boxRef = useRef();
+  
+  useLayoutEffect(() => {
+    console.log(boxRef.current.offsetWidth);
+  }, []);
+
+  return <div ref={boxRef}>Hello</div>;
 }
